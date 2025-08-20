d12a7cf5b3b2585644a16edb778d30fd2a6fbc1f
Don Reniff <reniffd@oregonstate.edu>
2025-08-20 13:18:55 -0700

    Add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Link, Navigate, useRoutes } from 'react-router-dom';
+import './App.css';
+
+import { supabase } from './client.js';
+
+// Pages
+import ShowCreators from './pages/ShowCreators.jsx';
+import ViewCreator from './pages/ViewCreator.jsx';
+import AddCreator from './pages/AddCreator.jsx';
+import EditCreator from './pages/EditCreator.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [creators, setCreators] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  /*
+   * Fetches creators from db only once after the initial render of the component.
+   */
+  useEffect(() => {
+    async function fetchCreators() {
+      setLoading(true);
+      const { data, error } = await supabase
+        .from("creators")
+        .select("*")
+        .order("id", { ascending: true });
+
+      if (error) {
+        console.error(error);
+        setCreators([]);
+      } else {
+        // Set to data if it exists, else set to empty array
+        setCreators(data ?? []);
+      }
+      setLoading(false);
+    }
+
+    fetchCreators();
+  }, []);
+
+  const routes = useRoutes([
+    { path: "/", element: <ShowCreators creators={creators} loading={loading} /> },
+    { path: "/creators/new", element: <AddCreator /> },
+    { path: "/creators/:id", element: <ViewCreator /> },
+    { path: "/creators/:id/edit", element: <EditCreator /> },
+    { path: "*", element: <Navigate to="/" replace /> }
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='container'>
+      <nav className='nav'>
+        <Link to="/" className='brand'>💫 Creatorverse</Link>
+      </nav>
+      {routes}
+    </div>
+  );
 }
 
-export default App
+export default App;
