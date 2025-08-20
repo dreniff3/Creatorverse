8c39efd96a746524044af44076dc84d96141db3a
Don Reniff <reniffd@oregonstate.edu>
2025-08-20 09:48:26 -0700

    Add page to show all content creators

---
+++ src/pages/ShowCreators.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+
+import CreatorCard from '../components/CreatorCard';
+
+const ShowCreators = ({ creators, loading }) => {
+  if (loading) return <div className="center">Loading creators...</div>;
+  
+  return (
+    <div>
+      <div className="row" style={{ justifyContent: "space-between" }}>
+        <h2>All Creators</h2>
+        <Link className="btn primary" to="/creators/new">+ Add Creators</Link>
+      </div>
+
+      {(!creators || creators.length === 0 ? (
+        <div className='center'>
+          <p>No content creators yet. Add your first one!</p>
+          <Link className="btn primary" to="/creators/new">Create Creator</Link>
+        </div>
+      ) : (
+        <div className="grid">
+          {creators.map((creator) => (
+            <CreatorCard key={creator.id} creator={creator} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ShowCreators;
